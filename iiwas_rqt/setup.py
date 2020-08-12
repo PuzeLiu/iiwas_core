79917ddf681949cb4bb4ddce47d6146e2f55cfba
boris-il-forte <boris.ilpossente@hotmail.it>
2020-08-12 14:28:59 +0200

    Minor fixes to iiwas_rqt
    - fixed some wrong installation paths
    - marginal improvements in ui

--- iiwas_rqt/setup.py
+++ iiwas_rqt/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rqt_mypkg'],
+    packages=['iiwas_rqt'],
     package_dir={'': 'src'},
 )
 
