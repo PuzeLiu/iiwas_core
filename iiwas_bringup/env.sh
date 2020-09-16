8493d1a2c6b67f93376d49022145666e376b9d27
Puze Liu <lpztc@163.com>
2020-09-16 19:33:35 +0200

    Update the IP address in env.sh

--- iiwas_bringup/env.sh
+++ iiwas_bringup/env.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-source /opt/ros/kinetic/setup.bash
+source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash  #TODO change use your own workspace
 
-export ROS_IP=192.168.2.4
-# export ROS_MASTER_URI=http://192.168.2.37:11311  # IP of fermat
-export ROS_MASTER_URI=http://192.168.2.2:11311   # IP of legendre
+export ROS_IP=192.168.10.1
+export ROS_MASTER_URI=http://192.168.10.4:11311   # IP of fermat 
 
 exec "$@"
