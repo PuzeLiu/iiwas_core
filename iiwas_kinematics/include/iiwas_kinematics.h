491cd9cb37285cf8b5cb514d56acf1b2e033f339
Puze Liu <lpztc@163.com>
2020-12-04 00:52:35 +0100

    Implement the analytical jacobian

--- iiwas_kinematics/include/iiwas_kinematics.h
+++ iiwas_kinematics/include/iiwas_kinematics.h
@@ -18,21 +18,34 @@ namespace iiwas_kinematics {
 //        typedef Transform<double, 3, Affine> TransformMatrixType;
         typedef Matrix4d TransformMatrixType;
         typedef Matrix<double, NUM_OF_JOINTS, 1> JointArrayType;
+        typedef Matrix<double, 6, NUM_OF_JOINTS> JacobianType;
+        typedef Matrix<double, 3, NUM_OF_JOINTS> JacobianPosType;
+        typedef Matrix<double, 3, NUM_OF_JOINTS> JacobianRotType;
 
         Kinematics();
 
-        Kinematics(Vector3d& tcp_position, Quaterniond& tcp_quaternion);
+        Kinematics(const Vector3d& tcp_position, const Quaterniond& tcp_quaternion);
 
         void forward_kinematics(const JointArrayType& q, Vector3d& out_ee_pos, Quaterniond& out_ee_quad);
+        void jacobian(const JointArrayType& q, JacobianType& out_jacobian);
+        void jacobian_pos(const JointArrayType& q, JacobianPosType& out_jacobian);
+        void jacobian_rot(const JointArrayType& q, JacobianRotType& out_jacobian);
 
     private:
         void transform_i(double q, int i, TransformMatrixType& out_T);
 
     private:
+        double d_bs;     // Distance from base to shoulder
+        double d_se;     // Distance from shoulder to elbow
+        double d_ew;      // Distance from elbow to wrist
+        double d_wf;    // Distance from elbow to finger(tip)
+
         JointArrayType dh_a;
         JointArrayType dh_alpha;
         JointArrayType dh_d;
 
+        Vector3d tcp_pos;
+        Quaterniond tcp_quat;
         TransformMatrixType T_ee;
     };
 }
--- iiwas_kinematics/src/example.cpp
+++ iiwas_kinematics/src/example.cpp
@@ -16,17 +16,69 @@ int main(int argc, char* argv[]){
     std::srand((unsigned int) time(0));
 
     Vector3d ee_pos;
-    Quaterniond ee_quad;
+    Quaterniond ee_quat;
+    ee_pos.setRandom();
+    ee_quat.setIdentity();
 
-    q.setRandom();
     auto start = chrono::high_resolution_clock::now();
     for (int i = 0; i < 10000; ++i) {
-        kinematics.forward_kinematics(q, ee_pos, ee_quad);
+        q.setRandom();
+        kinematics.forward_kinematics(q, ee_pos, ee_quat);
     }
     auto finish = chrono::high_resolution_clock::now();
-    cout << chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count() / 10000. / 1.e6 << "ms\n";
-    cout << "Position: " << endl << ee_pos << endl;
-    cout << "Rotation: " << endl << ee_quad.coeffs() << endl;
-    cout << "Rotation Matrix: " << endl << ee_quad.matrix() << endl;
+    cout << "Forward Kinematics Time: " << chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count() / 10000. / 1.e6 << "ms\n";
+
+    Kinematics::JacobianPosType jacobian_lin;
+    start = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 10000; ++i) {
+        q.setRandom();
+        kinematics.jacobian_pos(q, jacobian_lin);
+    }
+    finish = chrono::high_resolution_clock::now();
+    cout << "Linear Jacobian Time: " << chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count() / 10000. / 1.e6 << "ms\n";
+
+
+    double jac_eps = 1e-6;
+    Kinematics::JacobianPosType jacobian_numerical;
+    Vector3d ee_pos_positive, ee_pos_negative;
+    Kinematics::JointArrayType q_positive, q_negative;
+    start = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 10000; ++i) {
+        q.setRandom();
+        for (int j = 0; j < NUM_OF_JOINTS; ++j) {
+            q_positive = q;
+            q_positive(j) += jac_eps;
+            kinematics.forward_kinematics(q_positive, ee_pos_positive, ee_quat);
+
+            q_negative = q;
+            q_negative(j) -= jac_eps;
+            kinematics.forward_kinematics(q_negative, ee_pos_negative, ee_quat);
+
+            jacobian_numerical.block<3, 1>(0, j) = (ee_pos_positive - ee_pos_negative) / 2 / (jac_eps);
+        }
+    }
+    finish = chrono::high_resolution_clock::now();
+    cout << "Linear Jacobian Numerical Time: " << chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count() / 10000. / 1.e6 << "ms\n";
+
+
+    Kinematics::JacobianRotType jacobian_rot;
+    start = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 10000; ++i) {
+        q.setRandom();
+        kinematics.jacobian_rot(q, jacobian_rot);
+    }
+    finish = chrono::high_resolution_clock::now();
+    cout << "Rotation Jacobian Time: " << chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count() / 10000. / 1.e6 << "ms\n";
+
+    Kinematics::JacobianType jacobian;
+    start = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 10000; ++i) {
+        q.setRandom();
+        kinematics.jacobian(q, jacobian);
+    }
+    finish = chrono::high_resolution_clock::now();
+    cout << "Total Jacobian Time: " << chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count() / 10000. / 1.e6 << "ms\n";
+
+
     return 0;
 }
