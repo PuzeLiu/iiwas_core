a8a98cd6c1d76c968b730baa7ce86a6d19acf813
Puze Liu <lpztc@163.com>
2021-03-29 17:20:29 +0200

    Test Inertia

--- iiwas_control/scripts/plot_pid_gravity_ff.py
+++ iiwas_control/scripts/plot_pid_gravity_ff.py
@@ -17,7 +17,7 @@ def read_bag(bag, joint_id):
         if topic in ["/iiwa_front/joint_position_trajectory_controller/state",
                      "/iiwa_front/joint_feedforward_trajectory_controller/state"]:
             if not start:
-                if abs(msg.desired.positions[joint_id]) > 1e-3:
+                if abs(msg.desired.positions[joint_id]) > 1e-2:
                     start = True
                 else:
                     continue
@@ -40,35 +40,36 @@ def read_bag(bag, joint_id):
 
 
 if __name__ == '__main__':
-    bag_pid = rosbag.Bag(os.path.join("/home/puze/Desktop/real_trajectory/feedforward/Joint_1", "pid_0" + ".bag"))
+    bag_1 = rosbag.Bag(os.path.join("/home/puze/air_hockey_record/test_inertia", "inertia_1" + ".bag"))
     # bag_grav = rosbag.Bag(os.path.join("/home/puze/air_hockey_record/gravComp_Vs_FF/Joint_4", "position_pid_gravity" + ".bag"))
-    bag_ff = rosbag.Bag(os.path.join("/home/puze/Desktop/real_trajectory/feedforward/Joint_1", "pid_ff_0" + ".bag"))
+    bag_10 = rosbag.Bag(os.path.join("/home/puze/air_hockey_record/test_inertia", "inertia_10" + ".bag"))
+    bag_100 = rosbag.Bag(os.path.join("/home/puze/air_hockey_record/test_inertia", "inertia_100" + ".bag"))
 
-    joint_id = 0
+    joint_id = 3
 
-    position_pid, velocity_pid, acceleration_pid, effort_pid = read_bag(bag_pid, joint_id)
-    # position_grav, velocity_grav, effort_grav = read_bag(bag_grav)
-    position_ff, velocity_ff, acceleration_ff, effort_ff = read_bag(bag_ff, joint_id)
+    position_1, velocity_1, acceleration_1, effort_1 = read_bag(bag_1, joint_id)
+    position_10, velocity_10, acceleration_10, effort_10 = read_bag(bag_10, joint_id)
+    position_100, velocity_100, acceleration_100, effort_100 = read_bag(bag_100, joint_id)
 
     ax = plt.subplot()
-    ax.plot(position_pid[:, 2], label='pid')
-    # ax.plot(np.abs(position_grav[:, 2]), label='pid + gravity')
-    ax.plot(position_ff[:, 2], label='pid + ff')
+    ax.plot(position_1[:, 2], label='1')
+    ax.plot(position_10[:, 2], label='10')
+    ax.plot(position_100[:, 2], label='100')
     ax.legend()
     ax.title.set_text('error')
 
     fig, ax = plt.subplots(1)
-    ax.plot(position_pid[:, 1], label='pid')
-    # ax.plot(np.abs(position_grav[:, 2]), label='pid + gravity')
-    ax.plot(position_ff[:, 1], label='pid + ff')
-    ax.plot(position_ff[:, 0], label='desired')
+    ax.plot(position_1[:, 1], label='1')
+    ax.plot(position_10[:, 1], label='10')
+    ax.plot(position_100[:, 1], label='10')
+    ax.plot(position_10[:, 0], label='desired')
     ax.legend()
-    ax.title.set_text('actual')
+    ax.title.set_text('position')
 
     fig, ax = plt.subplots(1)
-    ax.plot(effort_pid, label='pid')
-    # axes[0].plot(effort_grav, label='pid + gravity')
-    ax.plot(effort_ff, label='pid + ff')
+    ax.plot(effort_1, label='1')
+    ax.plot(effort_10, label='10')
+    ax.plot(effort_100, label='100')
     ax.legend()
     ax.title.set_text('torque')
     plt.show()
